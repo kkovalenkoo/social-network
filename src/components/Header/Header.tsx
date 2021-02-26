757d246d413b8884dae2fe736cfeb14f501a1742
Yury Kovalenko <yvkovalenko1987@gmail.com>
2021-02-26 08:28:39 +0300

    12 breakdown into components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Header() {
+    return (
+            <header className='header'>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZzxV0K3qxDgsnn7LxcALK59V99QMj_veKIw&usqp=CAU"
+                    alt="logo"/>
+            </header>
+    );
+}
+
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export function Profile() {
+    return (
+            <div className='content'>
+                <img src="https://img.gazeta.ru/files3/173/6408173/1ca3f5-pic700-700x467-84454.jpg"/>
+                <div>ava + description</div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+    );
+}
+

1510ab61a4f4e55eff35d9652110dee07cb42db1
Yury Kovalenko <yvkovalenko1987@gmail.com>
2021-03-01 17:13:25 +0300

    21 layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,17 @@ import s from './Dialogs.module.css'
 
 export function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItem}>
+                <div>Yury</div>
+                <div>Sergey</div>
+                <div>Sasha</div>
+            </div>
+            <div className={s.messages}>
+                <div>Hello</div>
+                <div>Hi</div>
+                <div>Yo</div>
+            </div>
         </div>
     )
 }

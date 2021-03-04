d8e95cebdd18a3006b2a60529a2cc20bd353fa18
Yury Kovalenko <yvkovalenko1987@gmail.com>
2021-03-04 15:25:20 +0300

    24 UI - BLL

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,18 +1,35 @@
 import s from './Dialogs.module.css';
 import {NavLink} from 'react-router-dom';
 
+type DialogsDataType = {
+    id: number
+    name: string
+}
+type MessagesDataType = {
+    id: number
+    message: string
+}
+
 export function Dialogs() {
+
+    const dialogsData: Array<DialogsDataType> = [
+        {id: 1, name: 'Yury'},
+        {id: 2, name: 'Sergey'},
+        {id: 3, name: 'Sasha'}
+    ]
+    const messagesData: Array<MessagesDataType> = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'Hi'},
+        {id: 3, message: 'Yo'}
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItem}>
-                <DialogItem id={1} name='Yury'/>
-                <DialogItem id={2} name='Sergey'/>
-                <DialogItem id={3} name='Sasha'/>
+                {dialogsData.map(d => <DialogItem key={d.id} id={d.id} name={d.name}/>)}
             </div>
             <div className={s.messages}>
-                <Massages message='Hello'/>
-                <Massages message='Hi'/>
-                <Massages message='Yo'/>
+                {messagesData.map(m => <Messages key={m.id} message={m.message}/>)}
             </div>
         </div>
     );
@@ -34,7 +51,7 @@ function DialogItem(props: DialogsItemPropsType) {
     );
 }
 
-function Massages(props: MessagesPropsType) {
+function Messages(props: MessagesPropsType) {
     return (
         <div>{props.message}</div>
     );

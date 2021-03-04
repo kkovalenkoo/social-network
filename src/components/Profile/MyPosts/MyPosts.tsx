bc620763b65ae46d8ff031774a14196d1629481f
Yury Kovalenko <yvkovalenko1987@gmail.com>
2021-03-04 15:33:25 +0300

    24 UI - BLL

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import {Post} from './Post/Post';
-import s from './MyPosts.module.css'
+import s from './MyPosts.module.css';
+
+type postDataType = {
+    avatar: string
+    post: string
+    like: number
+}
 
 export function MyPosts() {
+
+    const postData: Array<postDataType> = [
+        {
+            avatar: 'https://ss.sport-express.ru/userfiles/materials/156/1564657/large.jpg',
+            post: `It's my first post`,
+            like: 10
+        },
+        {
+            avatar: `https://ss.sport-express.ru/userfiles/materials/156/1564657/large.jpg`,
+            post: 'Hello, how are you',
+            like: 5
+        }
+    ];
+
     return (
         <div>
             <div className={s.myPostsBlock}>
@@ -14,15 +34,9 @@ export function MyPosts() {
                     <button>Add Post</button>
                 </div>
             </div>
-            <Post avatar={'https://ss.sport-express.ru/userfiles/materials/156/1564657/large.jpg'}
-                  post={`It's my first post`}
-                  like={10}
-            />
-            <Post
-                avatar={`https://lh3.googleusercontent.com/proxy/fSdpiD-cVtH9rk236jSYbYZLVh5llddS51XlZtd6ETYCqWCf4RfGRRn43_uVYT2e866h4ulhYQ7HI54TclVpr2X6K2_A5-bJ--eB4mA7ZK-Q2W_OXTZ_cjp13BJRVQ`}
-                post={`Hello, how are you`}
-                like={5}
-            />
+            <div>
+                {postData.map(p => <Post post={p.post} like={p.like} avatar={p.avatar}/>)}
+            </div>
         </div>
     );
 }

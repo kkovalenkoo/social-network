75b7d5ce6a943e7b533ed2063f05e3891a020311
Yury Kovalenko <yvkovalenko1987@gmail.com>
2021-03-20 17:19:41 +0300

    37 store, bind

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import {addPost, inputNewTextForPost, state, subscribe} from './state/state';
+import {store} from './state/state';
 import ReactDOM from 'react-dom';
 import {App} from './App';
 import {BrowserRouter} from 'react-router-dom';
@@ -8,14 +8,16 @@ import {BrowserRouter} from 'react-router-dom';
 function rerenderEntireTree() {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} inputNewTextForPost={inputNewTextForPost}/>
+            <App state={store.getState()}
+                 addPost={store.addPost.bind(store)}
+                 inputNewTextForPost={store.inputNewTextForPost.bind(store)}/>
         </BrowserRouter>
         , document.getElementById('root'));
 }
 
 rerenderEntireTree();
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 
 
--- src/state/state.ts
+++ src/state/state.ts
@@ -1,5 +1,3 @@
-let rerenderEntireTree = () => {};
-
 export type UserType = {
     id: number
     name: string
@@ -24,48 +22,51 @@ export type StateType = {
     newTextForPost: string
 }
 
-export const state: StateType = {
-    dialogsData: {
-        users: [
-            {id: 1, name: 'Yury'},
-            {id: 2, name: 'Sergey'},
-            {id: 3, name: 'Sasha'}
-        ],
-        messages: [
-            {id: 1, message: 'Hello'},
-            {id: 2, message: 'Hi'},
-            {id: 3, message: 'Yo'}
-        ]
-    },
-    postsData: [
-        {
-            id: 1,
-            avatar: 'https://ss.sport-express.ru/userfiles/materials/156/1564657/large.jpg',
-            post: `It's my first post`,
-            like: 10
+export const store = {
+    _state: {
+        dialogsData: {
+            users: [
+                {id: 1, name: 'Yury'},
+                {id: 2, name: 'Sergey'},
+                {id: 3, name: 'Sasha'}
+            ],
+            messages: [
+                {id: 1, message: 'Hello'},
+                {id: 2, message: 'Hi'},
+                {id: 3, message: 'Yo'}
+            ]
         },
-        {
-            id: 2,
-            avatar: `https://ss.sport-express.ru/userfiles/materials/156/1564657/large.jpg`,
-            post: 'Hello, how are you',
-            like: 5
-        }
-    ],
-    newTextForPost: ''
+        postsData: [
+            {
+                id: 1,
+                avatar: 'https://ss.sport-express.ru/userfiles/materials/156/1564657/large.jpg',
+                post: `It's my first post`,
+                like: 10
+            },
+            {
+                id: 2,
+                avatar: `https://ss.sport-express.ru/userfiles/materials/156/1564657/large.jpg`,
+                post: 'Hello, how are you',
+                like: 5
+            }
+        ],
+        newTextForPost: ''
+    },
+    getState() {
+        return this._state;
+    },
+    rerenderEntireTree() {},
+    addPost() {
+        const newPost: PostDataType = {id: 3, avatar: '', post: this._state.newTextForPost, like: 0};
+        this._state.postsData.push(newPost);
+        this._state.newTextForPost = '';
+        this.rerenderEntireTree();
+    },
+    inputNewTextForPost(newTextForPost: string) {
+        this._state.newTextForPost = newTextForPost;
+        this.rerenderEntireTree();
+    },
+    subscribe(observer: () => void) {
+        this.rerenderEntireTree = observer;
+    }
 };
-
-export function addPost() {
-    const newPost: PostDataType = {id: 3, avatar: '', post: state.newTextForPost, like: 0};
-    state.postsData.push(newPost);
-    state.newTextForPost = '';
-    rerenderEntireTree();
-}
-
-export function inputNewTextForPost(newTextForPost: string) {
-    state.newTextForPost = newTextForPost;
-    rerenderEntireTree();
-}
-
-export function subscribe(observer: () => void) {
-    rerenderEntireTree = observer;
-}

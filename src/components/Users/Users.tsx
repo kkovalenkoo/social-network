8b2dd4d4a93e4ae0e0d644630966ec04a42f3414
Yury Kovalenko <yvkovalenko1987@gmail.com>
2021-04-08 12:54:19 +0300

    54 componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import {MapStateAndDispatchPropsType} from './UsersContainer';
 import axios from 'axios';
-import userPhoto from '../../photos/userPhoto.png'
+import userPhoto from '../../photos/userPhoto.png';
 
-export function Users(props: MapStateAndDispatchPropsType) {
+export class Users extends React.Component<MapStateAndDispatchPropsType> {
 
-    if (props.state.users.length === 0) {
-
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+            this.props.setUsers(response.data.items);
+        });
     }
 
-    return (
-        <div>
-            {props.state.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {this.props.state.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small ? u.photos.small : userPhoto}/>
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                            : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                            ? <button onClick={() => {
+                                this.props.unfollow(u.id);
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                this.props.follow(u.id);
+                            }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -35,7 +39,8 @@ export function Users(props: MapStateAndDispatchPropsType) {
                         <div>{'u.location.city'}</div>
                     </span>
                 </span>
-            </div>)}
-        </div>
-    )
+                </div>)}
+            </div>
+        );
+    }
 }
